f6285bb89dd2468dc3c87bbd766fc183c11e7400
alien087 <44940977+alien087@users.noreply.github.com>
2020-05-13 11:21:58 +0700

    connection to mongodb FIX!

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const connectDB = require('./config/db');
 
 const app = express();
 
-connectDB();
+
 
 app.get('/', (req, res) => res.send('API Running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+connectDB();
