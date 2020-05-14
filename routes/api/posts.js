059c3a22d1139d4d35f97102b86cb707d69db2a6
alien087 <44940977+alien087@users.noreply.github.com>
2020-05-14 11:14:18 +0700

    add post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,10 +1,43 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET API/posts
-// @desc    Test Route
-// Route    untuk user nambah apapun 
-// @access  Publoc (Don't need token)
-router.get('/', (req , res) => res.send('Posts Route'));
+const Post = require("../../models/Posts");
+const Profile = require("../../models/Profile");
+const User = require("../../models/user");
+
+// @route   POST API/posts
+// @desc    Tambah Postingan
+// @access  Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is Required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
+);
 
 module.exports = router;
