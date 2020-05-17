8db43e2a9cb74fa616285d2462eb6498689e1d21
alien087 <44940977+alien087@users.noreply.github.com>
2020-05-17 10:16:14 +0700

    comment add dan delete

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,8 @@ import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
+import CommentForm from '../post/CommentFrom';
+import CommentItem from '../post/CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
@@ -20,6 +22,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Back to post
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -5,6 +5,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -27,7 +29,7 @@ export default function (state = initialState, action) {
     case GET_POST:
       return {
         ...state,
-        posts: payload,
+        post: payload,
         loading: false,
       };
     case ADD_POST:
@@ -55,6 +57,23 @@ export default function (state = initialState, action) {
           post._id === payload ? { ...post, likes: payload.likes } : post
         ),
         loading: false,
+      };
+      case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            comment => comment._id !== payload
+          )
+        },
+        loading: false
       };
     default:
       return state;
